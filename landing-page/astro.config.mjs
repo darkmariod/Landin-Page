a01186bb9217ebbed9b8530d42d3ca01a648f1f3
Mario <109123293+darkmariod@users.noreply.github.com>
2024-12-20 12:31:02 -0500

    Commit
    First commit

---
+++ landing-page/astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'http://localhost:4321', // Asegúrate de que esto coincida con tu URL local
+  integrations: [],
+});
